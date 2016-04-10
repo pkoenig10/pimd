2c55462254fc245d9ae2c56edd00694d509807a4
ggoda <nhoran@andrew.cmu.edu>
2016-04-10 18:35:12 -0400

    Changed to Object Oriented Model
    - Nate

--- PiMD.h
+++ PiMD.h
@@ -6,24 +6,26 @@
  * Design considerations: OOP? < will make some things easier
  */
 
-/*
- * void init_gpu(): 
- * returns a object to refer to the gpu
- */
-void init_gpu ();
-/*
- * Load_mem (); 
- * takes in a pointer to memory and size and loads to memory.
- * Also takes in a gpu handler if not oop
- * will lock memory 
- */
-void load_mem ();
-/*
- * Store_mem ();
- * Takes in a ptr and a handle and moves bytes 
- * from gpu to host ptr.
- * Releases memory
- */
-void store_mem();
+class PiMD_GPU{
+    /*
+     * void init_gpu(): 
+     * returns a object to refer to the gpu
+     */
+    virtual void init_gpu ();
+    /*
+     * Load_mem (); 
+     * takes in a pointer to memory and size and loads to memory.
+     * Also takes in a gpu handler if not oop
+     * will lock memory 
+     */ 
+    virtual void load_mem ();
+    /*
+     * Store_mem ();
+     * Takes in a ptr and a handle and moves bytes 
+     * from gpu to host ptr.
+     * Releases memory
+     */
+    virtual void store_mem(); 
+}
 
 #endif
